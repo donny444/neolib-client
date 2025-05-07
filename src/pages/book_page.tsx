bb227605577b103e068cbe10a94cf75881d56c3b
donny_444 <tanadornrach@gmail.com>
2025-05-07 22:40:47 +0700

    take response of json instead of html

--- src/pages/book_page.tsx
+++ src/pages/book_page.tsx
@@ -8,11 +8,11 @@ import DeleteBook from "../utils/deletebook";
 import { useAuth } from "../contexts/auth_context";
 
 export default function BookPage(): JSX.Element {
-  const [data, setData] = useState<string>("");
+  // const [data, setData] = useState<string>("");
   const [edit, setEdit] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
   const [book, setBook] = useState<BookType>({} as unknown as BookType);
-  const uuid = useParams().uuid as string;
+  const isbn = useParams().isbn as string;
   const isAuthenticated = useAuth()?.isAuthenticated;
   const navigate = useNavigate();
 
@@ -26,7 +26,7 @@ export default function BookPage(): JSX.Element {
     const fetchBook = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:5000/books/${uuid}/`,
+          `http://localhost:5000/books/${isbn}/`,
           {
             headers: {
               "Access-Control-Allow-Origin": "*",
@@ -38,7 +38,7 @@ export default function BookPage(): JSX.Element {
           navigate("/");
         }
         const data = response.data;
-        setData(data);
+        setBook(data);
       } catch (error) {
         console.error("Error fetching data:", error);
         navigate("/");
@@ -46,16 +46,26 @@ export default function BookPage(): JSX.Element {
     };
 
     fetchBook();
-  }, [uuid, navigate]);
+  }, [isbn, navigate]);
 
   return (
     <>
-      <div dangerouslySetInnerHTML={{ __html: data }} />
-      {data ?
+      {/* <div dangerouslySetInnerHTML={{ __html: data }} /> data from HTML response */ }
+      <div>
+        <h3>{book.title}</h3>
+        <h3>ISBN: {book.isbn}</h3>
+        <p>Publisher: {book.publisher}</p>
+        <p>Category: {book.category}</p>
+        <p>Author: {book.author}</p>
+        <p>Pages: {book.pages}</p>
+        <p>Language: {book.language}</p>
+        <p>Publication Year: {book.publication_year}</p>
+      </div>
+      {book ?
         <>
           <button onClick={() => setEdit(!edit)}>{edit ? "Close Edit" : "Edit Book"}</button>
           <button onClick={async (e) => {
-            DeleteBook(e, uuid)
+            DeleteBook(e, isbn)
             navigate("/");
           }}>
             Delete Book
@@ -68,11 +78,11 @@ export default function BookPage(): JSX.Element {
           <InputField property="publisher" type="text" book={book} setBook={setBook} />
           <InputField property="category" type="select" book={book} setBook={setBook} />
           <InputField property="author" type="text" book={book} setBook={setBook} />
-          <InputField property="page" type="number" book={book} setBook={setBook} />
+          <InputField property="pages" type="number" book={book} setBook={setBook} />
           <InputField property="publication_year" type="number" book={book} setBook={setBook} />
           <InputField property="isbn" type="text" book={book} setBook={setBook} />
           <input type="submit" value="Submit" onSubmit={async (e) => {
-            const result = await EditBook(e, book, uuid);
+            const result = await EditBook(e, book, isbn);
 
             if (result) {
               setBook(undefined as unknown as BookType);
