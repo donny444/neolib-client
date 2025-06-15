e0ac14250b296663315f70c907a748cc946b6d1c
Donny <tanadornrach@gmail.com>
2025-06-16 03:27:54 +0700

    new component update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,9 +8,12 @@ export default function NavBar() {
   return (
     <div className="navbar navbar-expand-lg navbar-primary bg-success mb-5 p-2">
       {isAuthenticated ? (
-          <button className="d-flex justify-content-end gap-3 text-dark bg-light px-3 py-2 rounded-2" onClick={signout}>
-            Sign Out
-          </button>
+          <>
+            <Link to="/profile"></Link>
+            <button className="d-flex justify-content-end gap-3 text-dark bg-light px-3 py-2 rounded-2" onClick={signout}>
+              Sign Out
+            </button>
+          </>
       ) : (
         <div
           className="container d-flex justify-content-end gap-3"
