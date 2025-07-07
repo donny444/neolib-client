00d06d6f41f2d8bdddcecc1ab1003bfdf8834e19
Donny <tanadornrach@gmail.com>
2025-07-07 15:44:55 +0700

    omit scss deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: './dist',
+    outDir: "./dist",
   },
   server: {
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     port: 5173,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "import",
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+        ],
+      },
+    },
+  },
   plugins: [react()],
-})
+});
